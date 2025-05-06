53d30f88f9ff26a7ffbb2147ed9c6bfdd0f01d57
Kaz WATANABE <kaz@decr.jp>
2025-05-06 15:07:04 +0900

    アトリビュートが正常に生成されているかのテストを追加

--- tests/TestCase/Command/OpenApiControllerCommandTest.php
+++ tests/TestCase/Command/OpenApiControllerCommandTest.php
@@ -26,6 +26,11 @@ class OpenApiControllerCommandTest extends TestCase
     use ConsoleIntegrationTestTrait;
 
     /**
+     * @var array
+     */
+    private $loadedClasses = [];
+
+    /**
      * setUp method
      *
      * @return void
@@ -43,21 +48,7 @@ class OpenApiControllerCommandTest extends TestCase
             define('Cake\TwigView\View\CACHE', TMP . 'twig' . DS);
         }
 
-        // テスト用のコントローラファイルを削除
-        $paths = [
-            TEST_APP . 'Controller' . DS . 'ArticlesController.php',
-            TEST_APP . 'Controller' . DS . 'Admin' . DS . 'ArticlesController.php',
-        ];
-        foreach ($paths as $path) {
-            if (file_exists($path)) {
-                unlink($path);
-            }
-            // Adminディレクトリが存在する場合は、空のディレクトリも削除
-            $adminDir = dirname($path);
-            if (basename($adminDir) === 'Admin' && is_dir($adminDir) && count(glob($adminDir . '/*')) === 0) {
-                rmdir($adminDir);
-            }
-        }
+        $this->cleanupTestFiles();
     }
 
     public function tearDown(): void
@@ -65,6 +56,71 @@ class OpenApiControllerCommandTest extends TestCase
         parent::tearDown();
         Configure::delete('App.namespace');
         Plugin::getCollection()->remove('OpenApiTheme');
+        $this->cleanupTestFiles();
+    }
+
+    /**
+     * テストファイルをクリーンアップ
+     */
+    private function cleanupTestFiles(): void
+    {
+        $testFiles = glob(TEST_APP . 'Controller' . DS . '*Controller.php');
+        $testAdminFiles = glob(TEST_APP . 'Controller' . DS . 'Admin' . DS . '*Controller.php');
+        
+        foreach (array_merge($testFiles ?? [], $testAdminFiles ?? []) as $file) {
+            // AppController.phpは削除しない
+            if (basename($file) === 'AppController.php') {
+                continue;
+            }
+            
+            if (file_exists($file)) {
+                unlink($file);
+            }
+        }
+
+        $adminDir = TEST_APP . 'Controller' . DS . 'Admin';
+        if (is_dir($adminDir) && count(glob($adminDir . '/*')) === 0) {
+            rmdir($adminDir);
+        }
+    }
+
+    /**
+     * クラスをアンロードする
+     *
+     * @param string $className
+     * @return void
+     */
+    private function removeClass(string $className): void
+    {
+        $classExists = class_exists($className, false);
+        if ($classExists) {
+            $class = new ReflectionClass($className);
+            $fileName = $class->getFileName();
+            
+            if ($fileName && is_file($fileName)) {
+                opcache_invalidate($fileName, true);
+                opcache_reset();
+            }
+        }
+    }
+
+    /**
+     * クラスをロードする
+     *
+     * @param string $controllerPath
+     * @param string $className
+     * @return ReflectionClass
+     */
+    private function loadControllerClass(string $controllerPath, string $className): ReflectionClass
+    {
+        if (class_exists($className, false)) {
+            $this->removeClass($className);
+        }
+
+        require $controllerPath;
+        $this->loadedClasses[] = $className;
+        
+        return new ReflectionClass($className);
     }
 
     /**
@@ -74,19 +130,17 @@ class OpenApiControllerCommandTest extends TestCase
      */
     public function testBasicBaking(): void
     {
-        $this->exec('bake controller Articles --connection default --theme OpenApiTheme --no-test');
+        $this->exec('bake controller BasicArticles --connection default --theme OpenApiTheme --no-test');
         $this->assertExitSuccess();
-        $this->assertOutputContains('Baking controller class for Articles');
+        $this->assertOutputContains('Baking controller class for BasicArticles');
         $this->assertOutputContains('<success>Wrote</success>');
-        $this->assertOutputContains(TEST_APP . 'Controller' . DS . 'ArticlesController.php');
 
-        $controllerPath = TEST_APP . 'Controller' . DS . 'ArticlesController.php';
+        $controllerPath = TEST_APP . 'Controller' . DS . 'BasicArticlesController.php';
         $this->assertFileExists($controllerPath);
         
-        require_once $controllerPath;
-        $className = 'TestApp\\Controller\\ArticlesController';
+        $className = 'TestApp\\Controller\\BasicArticlesController';
+        $reflClass = $this->loadControllerClass($controllerPath, $className);
         
-        $reflClass = new ReflectionClass($className);
         $indexMethod = $reflClass->getMethod('index');
         $this->assertNotEmpty(
             $indexMethod->getAttributes(Get::class),
@@ -125,11 +179,46 @@ class OpenApiControllerCommandTest extends TestCase
      */
     public function testBakeWithPrefix(): void
     {
-        $this->exec('bake controller Articles --prefix Admin --connection default --theme OpenApiTheme --no-test');
+        $this->exec('bake controller PrefixArticles --prefix Admin --connection default --theme OpenApiTheme --no-test');
         $this->assertExitSuccess();
-        $this->assertOutputContains('Baking controller class for Articles');
+        $this->assertOutputContains('Baking controller class for PrefixArticles');
         $this->assertOutputContains('<success>Wrote</success>');
-        $this->assertOutputContains(TEST_APP . 'Controller' . DS . 'Admin' . DS . 'ArticlesController.php');
+
+        $controllerPath = TEST_APP . 'Controller' . DS . 'Admin' . DS . 'PrefixArticlesController.php';
+        $this->assertFileExists($controllerPath);
+        
+        $className = 'TestApp\\Controller\\Admin\\PrefixArticlesController';
+        $reflClass = $this->loadControllerClass($controllerPath, $className);
+        
+        $indexMethod = $reflClass->getMethod('index');
+        $this->assertNotEmpty(
+            $indexMethod->getAttributes(Get::class),
+            'Admin/indexメソッドにGetアトリビュートが存在しません'
+        );
+
+        $viewMethod = $reflClass->getMethod('view');
+        $this->assertNotEmpty(
+            $viewMethod->getAttributes(Get::class),
+            'Admin/viewメソッドにGetアトリビュートが存在しません'
+        );
+
+        $addMethod = $reflClass->getMethod('add');
+        $this->assertNotEmpty(
+            $addMethod->getAttributes(Post::class),
+            'Admin/addメソッドにPostアトリビュートが存在しません'
+        );
+
+        $editMethod = $reflClass->getMethod('edit');
+        $this->assertNotEmpty(
+            $editMethod->getAttributes(Put::class),
+            'Admin/editメソッドにPutアトリビュートが存在しません'
+        );
+
+        $deleteMethod = $reflClass->getMethod('delete');
+        $this->assertNotEmpty(
+            $deleteMethod->getAttributes(Delete::class),
+            'Admin/deleteメソッドにDeleteアトリビュートが存在しません'
+        );
     }
 
     /**
@@ -139,11 +228,43 @@ class OpenApiControllerCommandTest extends TestCase
      */
     public function testBakeWithActions(): void
     {
-        $this->exec('bake controller Articles --actions index,view,add --connection default --theme OpenApiTheme --no-test');
+        $this->exec('bake controller ActionArticles --actions index,view,add --connection default --theme OpenApiTheme --no-test');
         $this->assertExitSuccess();
-        $this->assertOutputContains('Baking controller class for Articles');
+        $this->assertOutputContains('Baking controller class for ActionArticles');
         $this->assertOutputContains('<success>Wrote</success>');
-        $this->assertOutputContains(TEST_APP . 'Controller' . DS . 'ArticlesController.php');
+
+        $controllerPath = TEST_APP . 'Controller' . DS . 'ActionArticlesController.php';
+        $this->assertFileExists($controllerPath);
+        
+        $className = 'TestApp\\Controller\\ActionArticlesController';
+        $reflClass = $this->loadControllerClass($controllerPath, $className);
+        
+        $indexMethod = $reflClass->getMethod('index');
+        $this->assertNotEmpty(
+            $indexMethod->getAttributes(Get::class),
+            'indexメソッドにGetアトリビュートが存在しません'
+        );
+
+        $viewMethod = $reflClass->getMethod('view');
+        $this->assertNotEmpty(
+            $viewMethod->getAttributes(Get::class),
+            'viewメソッドにGetアトリビュートが存在しません'
+        );
+
+        $addMethod = $reflClass->getMethod('add');
+        $this->assertNotEmpty(
+            $addMethod->getAttributes(Post::class),
+            'addメソッドにPostアトリビュートが存在しません'
+        );
+
+        $this->assertFalse(
+            method_exists($className, 'edit'),
+            'editメソッドが存在してはいけません'
+        );
+        $this->assertFalse(
+            method_exists($className, 'delete'),
+            'deleteメソッドが存在してはいけません'
+        );
     }
 
     /**
@@ -153,10 +274,36 @@ class OpenApiControllerCommandTest extends TestCase
      */
     public function testBakeWithNoActions(): void
     {
-        $this->exec('bake controller Articles --no-actions --connection default --theme OpenApiTheme --no-test');
+        $this->exec('bake controller NoActionArticles --no-actions --connection default --theme OpenApiTheme --no-test');
         $this->assertExitSuccess();
-        $this->assertOutputContains('Baking controller class for Articles');
+        $this->assertOutputContains('Baking controller class for NoActionArticles');
         $this->assertOutputContains('<success>Wrote</success>');
-        $this->assertOutputContains(TEST_APP . 'Controller' . DS . 'ArticlesController.php');
+
+        $controllerPath = TEST_APP . 'Controller' . DS . 'NoActionArticlesController.php';
+        $this->assertFileExists($controllerPath);
+        
+        $className = 'TestApp\\Controller\\NoActionArticlesController';
+        $reflClass = $this->loadControllerClass($controllerPath, $className);
+        
+        $this->assertFalse(
+            method_exists($className, 'index'),
+            'indexメソッドが存在してはいけません'
+        );
+        $this->assertFalse(
+            method_exists($className, 'view'),
+            'viewメソッドが存在してはいけません'
+        );
+        $this->assertFalse(
+            method_exists($className, 'add'),
+            'addメソッドが存在してはいけません'
+        );
+        $this->assertFalse(
+            method_exists($className, 'edit'),
+            'editメソッドが存在してはいけません'
+        );
+        $this->assertFalse(
+            method_exists($className, 'delete'),
+            'deleteメソッドが存在してはいけません'
+        );
     }
 }
